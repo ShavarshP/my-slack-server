975fdc4f8aa4ebfa7303ed4adbc741700bfdd55f
ShavarshP <papoyan101097@mail.ru>
2021-09-18 18:12:05 +0400

    fix:bug

--- liveServer/liveChat.js
+++ liveServer/liveChat.js
@@ -3,14 +3,16 @@ const chatIo = (io) => {
   const chat = {};
   io.on("connection", (socket) => {
     socket.on("ROOM:CHAT", ({ msg, email, id }) => {
-      if (id) {
-        chat[email] = id;
-      } else {
-        const emails = JSON.parse(email);
-        emails.forEach((element) => {
-          socket.to(chat[element]).emit("ROOM:CHAT", msg);
-        });
-      }
+      try {
+        if (id) {
+          chat[email] = id;
+        } else {
+          const emails = JSON.parse(email);
+          emails.forEach((element) => {
+            socket.to(chat[element]).emit("ROOM:CHAT", msg);
+          });
+        }
+      } catch (error) {}
     });
   });
 };
