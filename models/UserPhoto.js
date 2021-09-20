cf7d51b2cc5ce8c9b79333beafaec65b2f1eed18
ShavarshP <papoyan101097@mail.ru>
2021-09-20 17:02:48 +0400

    add: user photo logics

--- models/UserPhoto.js
+++ models/UserPhoto.js
@@ -2,7 +2,7 @@ const { Schema, model, Types } = require("mongoose");
 
 const schema = new Schema({
   email: { type: String, required: true, unique: true },
-  photo: { type: String, default: "" },
+  photo: { type: String, default: "[]" },
 });
 
 module.exports = model("UserPhoto", schema);
